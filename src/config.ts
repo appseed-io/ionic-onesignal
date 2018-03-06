42d281221874d512b1b92039e003eabb3db73e8b
George Konispoliatis <konispoliatisg@gmail.com>
2018-03-06 17:55:50 +0200

    Add required keys

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ import { Injectable } from '@angular/core';
 export class Config {
 	public wordpressApiUrl = 'http://demo.titaniumtemplates.com/wordpress/?json=1';
 }
+
+export const sender_id = '150979894356';
+export const oneSignalAppId = 'fccaa84e-5d60-409c-826e-6f8bf5415acd';
